c5a41c590b7f864a4c52f09e43234fd0ab935af4
sandeep <sandeepsanu1230@gmail.com>
2025-11-03 15:48:14 +0530

    updated jwt

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: process.env.JWT_EXPIRES_IN || "1d",
   });
 };
 
